9ccefd42f1dd707945ff7c7aef048afd9dd425ec
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-11-03 11:17:47 -0300

    refactor: update navbar component to receive props

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,25 +1,35 @@
 import React from 'react'
 import { FiArrowLeft } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 
 import LogoImg from '../../assets/images/logo.png'
 
 import './styles.css'
 
-const Navbar = () => {
+const Navbar = (props) => {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <FiArrowLeft size={30} />
-                </li>
-                <li>
-                    Home
-                </li>
-                <li>
-                    <img src={LogoImg} alt="Find User"/>
-                </li>
-            </ul>
-        </nav>
+        <header className="page-header">
+            <nav className="page-header-navbar">
+                <ul>
+                    <li>
+                        <Link to="/">
+                            <FiArrowLeft size={30} />
+                        </Link>
+                    </li>
+                    <li className="page-header-navbar-title">
+                        Home
+                    </li>
+                    <li>
+                        <img src={LogoImg} alt="Find User" />
+                    </li>
+                </ul>
+            </nav>
+
+            <div className="header-content">
+                <strong>{props.description}</strong>
+                <p>{props.totalUsers} usuários</p>
+            </div>
+        </header>
     )
 }
 
